f8d3b0cae74e917187b6240f08c6baf25905f0b2
Wanjiru393 <carolciru393@gmail.com>
2022-05-10 01:33:57 +0300

    Added errorhandler

--- App/views.py
+++ App/views.py
@@ -12,3 +12,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html',articles=news) 
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'),404
