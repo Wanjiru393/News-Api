fe02d2331d0b461820333fbf943446c51d87eee7
Wanjiru393 <carolciru393@gmail.com>
2022-05-09 23:33:16 +0300

    Created everything class

--- App/models/news.py
+++ App/models/news.py
@@ -19,3 +19,17 @@ class Sources:
         self.url=url
         self.category=category
         self.country=country
+
+
+class Everything:
+    '''
+    Defining everything object that with pull based on category
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
