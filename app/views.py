ca3cb199692a4d5137b891c90cb989641e11add4
Wanjiru393 <carolciru393@gmail.com>
2022-05-10 02:07:36 +0300

    Polishing up

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from flask import render_template,url_for
 from app import app
-from .request import get_News
+from .request import get_news
 
 # Views
 @app.route('/')
 def index():
 
-    news=get_News()
+    news=get_news()
 
     '''
     View root page function that returns the index page and its data
